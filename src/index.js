7a0d3498ee83333289b603b61f98425ee5019e68
Your Name <samsulr99@gmail.com>
2022-07-22 15:46:12 +0700

    update react version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -7,11 +7,11 @@ import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
