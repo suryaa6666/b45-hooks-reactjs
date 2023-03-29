72db996fcefd4193d7821eebdc92aad215878905
suryaa6666 <suryaelidanto@gmail.com>
2023-03-29 11:04:26 +0700

    result

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Welcome extends Component {
+  //Create method DidMount inside it can print "Welcome Component Did Mount" here...
+  componentDidMount() {
+    console.log("Welcome componentDidMount");
+  }
 
-    //Create method DidMount inside it can print "Welcome Component Did Mount" here...
+  //Create method WillUnmount inside it can print "Welcome Component  Will Unmount" here...
+  componentWillUnmount() {
+    console.log("Welcome componentWillUnmount");
+  }
 
-    //Create method WillUnmount inside it can print "Welcome Component  Will Unmount" here...
-
-    render() {
-        return (
-            <div className="vh-100 bg-warning d-flex justify-content-center align-items-center h1 mb-0">
-                Welcome
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="vh-100 bg-warning d-flex justify-content-center align-items-center h1 mb-0">
+        Welcome
+      </div>
+    );
+  }
 }
